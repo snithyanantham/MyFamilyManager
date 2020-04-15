5facab7f8d153ca9af1c67ce1d01970595cd1f0a
snithyanantham <60456578+snithyanantham@users.noreply.github.com>
2020-04-15 21:49:33 +0530

    Dev (#1)
    * added Swash buckle based swagger doc and updated launch settings to root for launch
    
    * Update README.md
    
    * Update README.md
    
    * folder restructure to include all the application under single solution

--- src/Services/MyFamilyManager.API/Startup.cs
+++ src/Services/MyFamilyManager.API/Startup.cs
@@ -26,6 +26,10 @@ namespace myfamilymanager.api.host
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My Family Manager", Version = "v1" });
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +39,14 @@ namespace myfamilymanager.api.host
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Family Manager V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
