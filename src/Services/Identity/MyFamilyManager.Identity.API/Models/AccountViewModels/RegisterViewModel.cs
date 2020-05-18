a0ac5a9cbc43b6d80a8bd26ab4eac92fd43d02f5
Nithyanantham S <nithyananthamn@gmail.com>
2020-05-18 12:09:10 +0530

    Added new fields in register form

--- src/Services/Identity/MyFamilyManager.Identity.API/Models/AccountViewModels/RegisterViewModel.cs
+++ src/Services/Identity/MyFamilyManager.Identity.API/Models/AccountViewModels/RegisterViewModel.cs
@@ -4,17 +4,22 @@ namespace MyFamilyManager.Identity.API.Models.AccountViewModels
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "EMAIL_REQUIRED")]
-        [EmailAddress(ErrorMessage = "EMAIL_INVALID")]
+        [Required(ErrorMessage = "Email Required")]
+        [EmailAddress(ErrorMessage = "Email Invalid")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "PASSWORD_REQUIRED")]
+        [Required(ErrorMessage = "Password Required")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "CONFIRM_PASSWORD_NOT_MATCHING")]
+        [Compare("Password", ErrorMessage = "Confirm Password not Matching")]
         public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "First Name Required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name Required")]
+        public string LastName { get; set; }
     }
 }
