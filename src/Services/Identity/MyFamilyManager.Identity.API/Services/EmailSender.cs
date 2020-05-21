b123a4a97d9dac1b91dd37902ff8957fcd1fdb1b
Nithyanantham S <60456578+snithyanantham@users.noreply.github.com>
2020-05-21 20:01:33 +0530

    Dev (#5)
    * added Swashbuckle based swagger doc and updated launchsettings to root for launch
    
    * Update README.md
    
    * Update README.md
    
    * folder restructure to include all the aplication under single solution
    
    * Added solution for Identity API and MyFamilyManager web
    
    * Basic integration with API and web project, Added Package reference for MySQL db
    
    * Added docker compose support and integrated db
    
    * Corrected docker file to launch poperly and added reactjs docker compose
    
    * Added AspNetIdentity, IdentityServer4 db (ConfigurationDb, PersistedGrantDb) context added to IdentityServer project along with controllers
    
    * Update README.md
    
    * Added aspnetidentity for identity api
    
    * added client side libs
    
    * refactoring register page
    
    * added configuration to startup and also updated resource strings
    
    * Added new fields in register form
    
    * updated docker file for core api
    
    * Update workflow.yml
    
    * docker file with relative path
    
    * removed unused project
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * added unit test project

--- src/Services/Identity/MyFamilyManager.Identity.API/Services/EmailSender.cs
+++ src/Services/Identity/MyFamilyManager.Identity.API/Services/EmailSender.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Options;
 using MyFamilyManager.Identity.API.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System.Threading.Tasks;
 
 namespace MyFamilyManager.Identity.API.Services
@@ -15,17 +17,17 @@ namespace MyFamilyManager.Identity.API.Services
 
         public async Task SendEmail(string email, string subject, string message, string toUsername)
         {
-            //var client = new SendGridClient(_optionsEmailSettings.Value.SendGridApiKey);
-            //var msg = new SendGridMessage();
-            //msg.SetFrom(new EmailAddress(_optionsEmailSettings.Value.SenderEmailAddress, "damienbod"));
-            //msg.AddTo(new EmailAddress(email, toUsername));
-            //msg.SetSubject(subject);
-            //msg.AddContent(MimeType.Text, message);
-            ////msg.AddContent(MimeType.Html, message);
+            var client = new SendGridClient(_optionsEmailSettings.Value.SendGridApiKey);
+            var msg = new SendGridMessage();
+            msg.SetFrom(new EmailAddress(_optionsEmailSettings.Value.SenderEmailAddress, "damienbod"));
+            msg.AddTo(new EmailAddress(email, toUsername));
+            msg.SetSubject(subject);
+            msg.AddContent(MimeType.Text, message);
+            //msg.AddContent(MimeType.Html, message);
 
-            //msg.SetReplyTo(new EmailAddress(_optionsEmailSettings.Value.SenderEmailAddress, "damienbod"));
-            
-            //var response = await client.SendEmailAsync(msg);
+            msg.SetReplyTo(new EmailAddress(_optionsEmailSettings.Value.SenderEmailAddress, "damienbod"));
+
+            var response = await client.SendEmailAsync(msg);
         }
     }
 }
