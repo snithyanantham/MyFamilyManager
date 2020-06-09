2c51a38eb439b1948eaba485ea191273f2424305
Nithyanantham S <nithyananthamn@gmail.com>
2020-06-09 21:09:07 +0530

    Added seed data for identity tables

--- src/Services/Identity/MyFamilyManager.Identity.API/Startup.cs
+++ src/Services/Identity/MyFamilyManager.Identity.API/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -84,6 +86,7 @@ namespace MyFamilyManager.Identity.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                InitializeDatabase(app);
             }
             else
             {
@@ -108,5 +111,43 @@ namespace MyFamilyManager.Identity.API
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.Ids)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.Apis)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
     }
+
 }
